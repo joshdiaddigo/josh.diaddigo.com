5950040a5544a56f70410129aa6608a375ba8890
Joshua Diaddigo <joshuad95@hotmail.com>
2017-02-10 12:26:19 -0500

    adding google analytics

--- resources/js/main.js
+++ resources/js/main.js
@@ -5,6 +5,13 @@ window.onload = function() {
     terminal.setup();
     frosted_alert.setup();
     setup();
+
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-91788947-1', 'auto');
+    ga('send', 'pageview');
 };
 
 function setup() {
@@ -165,3 +172,4 @@ function move_mobile_tray(close) {
         nav.add_class("show_nav");
     }
 }
+
